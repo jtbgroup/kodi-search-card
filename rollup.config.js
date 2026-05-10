980ab65ebaae7938cee1701d771b03b203513c70
gautier vanderslyen <gautier.vanderslyen@gmail.com>
2026-05-10 21:12:35 +0000

    3.9.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,22 @@
-// ... tes autres imports
 import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import terser from '@rollup/plugin-terser';
+import serve from "rollup-plugin-serve";
+import json from "@rollup/plugin-json";
+
+const dev = process.env.ROLLUP_WATCH;
+
+const serveopts = {
+    contentBase: ["./dist"],
+    host: "0.0.0.0",
+    port: 5000,
+    allowCrossOrigin: true,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+    },
+};
 
 const plugins = [
     typescript({
@@ -22,9 +39,21 @@ const plugins = [
     babel({
         exclude: "node_modules/**",
         babelHelpers: "bundled",
-        // On s'assure que Babel comprend aussi les décorateurs
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
     }),
     dev && serve(serveopts),
     !dev && terser(),
+];
+
+export default [
+    {
+        input: "src/kodi-search-card.ts",
+        output: {
+            dir: "dist",
+            format: "es",
+            sourcemap: dev,
+        },
+        plugins: [...plugins],
+        context: "window",
+    },
 ];
